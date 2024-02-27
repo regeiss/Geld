c1b80ebd4bcb3369c189615f2bc065461640d85c
Roberto <roberto.geiss.nh@icloud.com>
2024-02-27 20:14:31 -0300

    Initial Commit

---
+++ Geld/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Geld
+//
+//  Created by Roberto Edgar Geiss on 27/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Geld/GeldApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeldApp.swift
+//  Geld
+//
+//  Created by Roberto Edgar Geiss on 27/02/24.
+//
+
+import SwiftUI
+
+@main
+struct GeldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
